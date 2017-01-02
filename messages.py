91ec4ca0e11166c5691350169b9c5eb24c662b6b
Mo'men Adel <mouemened@gmail.com>
2017-01-02 15:05:41 +0200

    Implement Message and MessageTemplates Base classes

---
+++ messages.py
@@ -0,0 +1,33 @@
+from  abc import ABC, abstractmethod
+
+class Message(ABC):
+	@abstractmethod
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def as_dict(self):
+		pass
+
+class TextMessage(Message):
+	def __init__(self, text):
+		# super().__init_)
+		self.text = text
+
+	def as_dict(self):
+		return {"text": text}
+
+class TemplateMessage(Message, ABC):
+	@abstractmethod
+	def __init__(self, template_type):
+		# super().__init_()
+		self.template_type = template_type
+
+	@abstractmethod
+	def as_dict(self):
+		return {
+			"attachment": {
+				"type": "template",
+				"payload": 	self.__dict__
+			}
+		}
